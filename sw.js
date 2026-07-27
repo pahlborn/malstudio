307995c0f6a422be0d9e1aae8a6eeb551c54de14
pahlborn <pahlborn@gmx.de>
2026-07-27 14:33:25 +0200

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,37 @@
+/* Service Worker – speichert die App fürs Offline-Malen */
+const CACHE = 'malstudio-v5-3';
+const ASSETS = [
+  './malstudio-v5.html',
+  './manifest.webmanifest',
+  './icon-180.png',
+  './icon-192.png',
+  './icon-512.png',
+  './icon-mask-512.png'
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)).then(() => self.skipWaiting()));
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+    ).then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch', e => {
+  if (e.request.method !== 'GET') return;
+  e.respondWith(
+    caches.match(e.request).then(hit => {
+      if (hit) return hit;
+      return fetch(e.request).then(res => {
+        // Google-Fonts u. Ä. für später mitcachen
+        const copy = res.clone();
+        caches.open(CACHE).then(c => c.put(e.request, copy)).catch(() => {});
+        return res;
+      }).catch(() => hit);
+    })
+  );
+});
